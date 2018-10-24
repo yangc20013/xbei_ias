54a34b8042344a0d06940944a1832e751b234cda
cheng1.yang <cheng1.yang@symbio.com>
2018-10-24 14:53:45 +0800

    update get data

--- python/index.py
+++ python/index.py
@@ -3,6 +3,7 @@
 import requests
 import datetime
 import json
+import random
 import tushare as ts
 
 max=0.50
@@ -22,16 +23,54 @@ def get_day_stock_info(code,bdate,edate):
 		bdate=n_days.strftime('%Y%m%d')
 	if edate==None:
 		edate=datetime.datetime.now().strftime('%Y%m%d')
-	headers=("user-agent","Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.22 Safari/537.36 SE 2.X MetaSr 1.0")
+	user_agent_list=[\
+					 "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 "  
+        "(KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",  
+        "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 "  
+        "(KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",  
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 "  
+        "(KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6",  
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 "  
+        "(KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6",  
+        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.1 "  
+        "(KHTML, like Gecko) Chrome/19.77.34.5 Safari/537.1",  
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 "  
+        "(KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5",  
+        "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 "  
+        "(KHTML, like Gecko) Chrome/19.0.1084.36 Safari/536.5",  
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 "  
+        "(KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",  
+        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 "  
+        "(KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",  
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/536.3 "  
+        "(KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",  
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 "  
+        "(KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",  
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 "  
+        "(KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",  
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 "  
+        "(KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",  
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 "  
+        "(KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",  
+        "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 "  
+        "(KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",  
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 "  
+        "(KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3",  
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 "  
+        "(KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",  
+        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 "  
+        "(KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"
+					]
+	headers=("user-agent",random.choice(user_agent_list))
 	url="http://q.stock.sohu.com/hisHq?code=cn_%s&start=%s&end=%s&stat=1&order=D&period=d&callback=historySearchHandler&rt=json"%(code,bdate,edate)
-	response = requests.get(url)
+	response = requests.get(url,headers=headers)
 	data = response.text
 	parse_data(code,data)
 
 def str2float(s):
 	try:
-		aa = float(s.strip('%'))
-		return aa/100.0
+		value = float(s.strip('%'))
+		return value/100.0
 	except Exception,e:
 		print(s,e)
 
